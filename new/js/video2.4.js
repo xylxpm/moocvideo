3543433418ec8f27b368593619125b27ffda9a41
xuyanyfb <xuyanyfb@etiantian.com>
2017-08-31 13:20:44 +0800

    嵌入jwqlayer

--- new/js/video2.4.js
+++ new/js/video2.4.js
@@ -11,28 +11,39 @@ $(function () {
 
     var j = {scrollTo: c()};
 
-    var createPlayerLisSection = function (videoUrl, imageUrl, width, height, videoDiv) {
-        jwplayer(videoDiv).setup({
-            'width': width,
-            'height': height,
-            'file': videoUrl,
-            'image': imageUrl,
-            'analytics': {
-                'cookies': false,
-                'enabled': false
-            },
-            'primary': "html5",
-            'preload': "none",
-            'androidhls': "true",
-            'abouttext': "Etiantian.com",
-            'aboutlink': "http://www.etiantian.com",
-            'autostart': "false",
-            'startparam': "start"
-        });
+    var playlist = [{ file: "../video/3m.mp4", image: "../new/img/1.jpg", title:'001' },
+        { file: "../video/3m.mp4", image: "../new/img/2.jpeg",title:'002'},
+        {file: "../video/3m.mp4",image: "../new/img/3.jpg", title:'003' }];
 
-    }
 
-    createPlayerLisSection("http://web.etiantian.com/ett20/hls/hd.m3u8?p=ceyw000026&s=b&t=1504075643&v=be5e5c8511a8d319fd38e90947d35ff1&h=http%3A%2F%2Fhd.etiantian.com", "http://hd.etiantian.net/security/c3a160887a1fd4517435693e8304d60f/59a660ca/etthd/ceyw000026/cover.jpg", "100%", "100%", 'videoDiv');
+    var thePlayer = jwplayer('videoDiv').setup({
+        width: "100%",
+        height: "100%",
+        file: "../video/3m.mp4",
+        image: "../new/img/1.jpg",
+        analytics: {
+            cookies: false,
+            enabled: false
+        },
+        //'playlist': [
+        //    { file: "../video/3m.mp4", image: "../new/img/1.jpg", title:'001' },
+        //    { file: "../video/3m.mp4", image: "../new/img/2.jpeg",title:'002'},
+        //    {file: "../video/3m.mp4",image: "../new/img/3.jpg", title:'003' }
+        //],
+        autostart :true,
+        primary: "html5",
+        preload: "none",
+        androidhls: true,
+        abouttext: "Etiantian.com",
+        aboutlink: "http://www.etiantian.com",
+        autostart: false,
+        startparam: "start"
+    });
+    //thePlayer.load(playlist);
+
+
+    //createPlayerLisSection("http://web.etiantian.com/ett20/hls/hd.m3u8?p=ceyw000026&s=b&t=1504075643&v=be5e5c8511a8d319fd38e90947d35ff1&h=http%3A%2F%2Fhd.etiantian.com", "http://hd.etiantian.net/security/c3a160887a1fd4517435693e8304d60f/59a660ca/etthd/ceyw000026/cover.jpg", "100%", "100%", 'videoDiv');
+
 
 
     var a = function () {
@@ -77,26 +88,32 @@ $(function () {
             $(window).trigger("scroll")
         }, 200)
     });
-    var h = $(".section-list");
+
     $(".chapter").on("click", function () {
         var c = $(this);
+        var h = $(".section-list");
         c.hasClass("light") ? (c.removeClass("light"), h.animate({right: -360}, 200)) : (c.addClass("light"), h.animate({right: 0}, 200)), a.resetSize()
     })
 
 
     $(".section-list .notes").on("click", function () {
-        window.thePlayer && window.thePlayer.getState && "PAUSED" != window.thePlayer.getState() && "IDLE" != window.thePlayer.getState() && thePlayer.pause();
+        if (thePlayer.getState() == 'PLAYING') {
+            thePlayer.play(false);
+        }
         $(".note-pop").length || (window.notepop = $.dialog($("#course-note-tpl").html(), {title: "笔记"}))
     });
 
     $(".section-list .wiki").on("click", function () {
-        window.thePlayer && window.thePlayer.getState && "PAUSED" != window.thePlayer.getState() && "IDLE" != window.thePlayer.getState() && thePlayer.pause();
-
-        $(".wiki-pop").length || (window.wikipop = $.dialog($("#course-wiki-tpl").html(), {title: "搜索WIKI"}))
+        if (thePlayer.getState() == 'PLAYING') {
+            thePlayer.play(false);
+        }
+        $(".wiki-pop").length || (window.wikipop = $.dialog($("#course-wiki-tpl").html(), {title: "WIKI"}))
     });
 
     $(".section-list .question").on("click", function () {
-        window.thePlayer && window.thePlayer.getState && "PAUSED" != window.thePlayer.getState() && "IDLE" != window.thePlayer.getState() && thePlayer.pause()
+        if (thePlayer.getState() == 'PLAYING') {
+            thePlayer.play(false);
+        }
         $.dialog($("#course-wiki-tpl").html(), {title: "提问"})
     });
 })
